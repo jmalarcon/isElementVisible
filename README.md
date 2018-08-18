77477a985a6645726b8f78d329df05ffd8d0a6d1
José M. Alarcón <jmalarcon@jasoft.org>
2018-08-18 20:09:39 +0200

    V 2.0.0

--- README.md
+++ README.md
@@ -1,35 +1,36 @@
-# isElementVisible
+# isElementVisible.js
 
-Something that can be very useful in a page or web application is **the ability to detect when a particular item appears or disappears from the screen** due to the actions of the user.
+Something that can be very useful in a page or web application is **the ability to detect when a particular item appears or disappears from the screen** due to the actions of the user. It's similar to the "waypoints" feature some libraries include, but in a generic and simpler way.
 
-For example, if a piece of important information disappears because the user scrolls by moving the contents, we can extract a summary note, reminder or shortcut to go see it again, and hide it again when the element reappears. Things like that...
+For example, if a piece of important information disappears because the user scrolls by moving the contents, we can extract a summary note, reminder or shortcut to go see it again, apply an animation, or hide it again when the element reappears. Things like that...
 
 To get something like this we could use **an `inViewport` event**, or similar, to get notified when an item appears or disappears from the visible part of the page (the viewport). We would subscribe to this event and automatically receive notifications if the item appears or disappears.
 
 The problem is that **there is no such event in HTML / JavaScript**.
 
-To fix this problem I have created the library `isElementVisible.js`.
+To fix this problem I have created this simple library `isElementVisible.js`, written in pure ECMAScript 5 and working in even older browsers.
 
-The library exposes **a `visibilityHelper` object** with 4 functions:
+The library exposes **a `visibilityHelper` global object** with 4 functions:
 
 - **`isElementTotallyVisible(element)`**: Indicates whether or not an element is within the visible area of ​​the page.
 - **`isElementPartiallyVisible(element)`**: Indicates whether or not an element is within the visible area of ​​the page.
-- **`inViewportPartially(element, handler)`**: It is used to define an event handler that will be automatically raised when the element that is indicated is partially or totally outside the current page.
-- **`inViewportTotally(element, handler)`**: It is used to define an event handler that will be automatically raised when the element that is indicated is completely in or out of the current page.
+- **`inViewportPartially(element, handler, onlyFirstTime)`**: It is used to define an event handler that will be automatically raised when the element that is indicated is partially or totally outside the current page. The last boolean parameter indicates if the handler is going to be called every time this happens (`false`, default value), or only the first time the element enters the viewport (`true`).
+- **`inViewportTotally(element, handler, onlyFirstTime)`**: It is used to define an event handler that will be automatically raised when the element that is indicated is completely in or out of the current page. The last boolean parameter indicates if the handler is going to be called every time this happens (`false`, default value), or only the first time the element enters the viewport (`true`).
 
 To use this library just put a reference to the script in the header of the page and then define the event in a similar way to this:
 
 ```
 var myDiv = document.getElementById("myElement");
-visibilityHelper.inViewportPartially(myDiv, function(visible, element){
-                                                console.log("%s element is visible?: %s", element.id, visible);
-                                        });
+visibilityHelper.inViewportPartially(myDiv, 
+        function(visible, element){
+                console.log("%s element is visible?: %s", element.id, visible);
+        });
 ```
 
-Which will cause the event handler to be called whenever the element partially enters or leaves the visible area of ​​the page.
+Which will cause the event handler to be called whenever the element partially enters or leaves the visible area of ​​the page, notifying the current visibility state and the element (in case we use the same handler for several elements).
 
->**Note**: The events definition should be done at the end of the page or in the load event (or `$.ready()` in jQuery), where the elements that we are going to monitor are fully defined and we can locate them.
+>**Note**: The events definition should be done at the end of the page or in the load event (or [`$.ready()`](https://api.jquery.com/ready/) in jQuery), where the elements that we are going to monitor are fully defined and we can locate them.
 
-In the repository is also included a small example (test.html) with two boxes stuck in the middle of a lot of text (to be able to scroll and test them). This example uses both events (total and partial visibility) to display **in the browser console** when the event of entry and exit of the visible area happens, and thus to be able to test it in practice.
+In the repository a small example is also included (`test.html`) with two boxes stuck in the middle of a lot of text (to be able to scroll and test them). This example uses both events (total and partial visibility) to display **in the browser console** when the event of entry and exit of the visible area happens, and thus to be able to test it in practice. Add a `true` value to the `inViewportTotally` or `inViewportPartially` methods in the test to test that the handler is being called only the first time.
 
 I hope you find it useful!
--- isElementVisible.js
+++ isElementVisible.js
@@ -1,11 +1,33 @@
 /**
- * @file This library defines an object with four functions to simulate a nonexistent event that allows to detect when any element of the page enters or leaves the visible area of the same.
+ * @file This library defines an object with two functions to detect visibility (total or partial) of an element in the viewport, and two functions that simulate two nonexistent events to detect when any element enters or leaves the visible area of the page.
  * @author José Manuel Alarcón - www.JASoft.org - www.campusMVP.es
- * @version 1.0
+ * @version 2.0
+ * @license MIT
  */
 
 (function(){
     
+    /**
+     * Regulating function that allows to imit the maximun number of calls to a function
+     * @param {function} func - The function to regulate
+     * @param {int} maxPerSec - Maximun number of tuimes per second the function can be called
+     */
+    function limitCalls(func, maxPerSec) {
+        var lockActivated = false;
+
+        return function() {
+            var disableLock = function() {
+                lockActivated = false;
+            };
+
+            if (!lockActivated) {
+                func.apply(this, arguments);
+                lockActivated = true;
+                setTimeout(disableLock, 1000/maxPerSec);
+            }
+        };
+    }
+
     /**
      * Determines whether the element passed as a parameter is fully visible in the browser window or not.
      * @param {object} elt - The element whose visibility we are interested in determining
@@ -43,46 +65,60 @@
      * Defines a handler to be able to automatically detect when a certain element changes visibility on the page. This version considers that the element is visible, if any part of it is in the viewport.
      * @param {object} elt - The element whose visibility we are interested in determining
      * @param {function} handler - The function that will be called when the element has changed its visibility state. It receives a boolean to indicate whether it is visible or not, and a reference to the element that has changed its visibility.
-     * @returns {boolean} Returns true if there is something (even a little) of the element in the visible area of the page, and returns false if the element disappears completely from the visible area.
+     * @param {boolean} onlyFirstTime - If true the handler is called only the first time it's detected. Otherwise it will be called everytime the element is visible.
      */
-    function inViewportPartially(elt, handler) {
+    function inViewportPartially(elt, handler, onlyFirstTime) {
         var prevVisibility = isElementPartiallyVisible(elt);    //Creates a closure for the handler of this particular event
         //Define a handler to determine possible changes
         function detectPossibleChange() {
             var isVisible = isElementPartiallyVisible(elt);
             if (isVisible != prevVisibility) { //the visibility state of the element has changed
+                if(onlyFirstTime) { //If it's only to be called the first time, remove the events after it
+                    window.removeEventListener("load", regulatedDetection);
+                    window.removeEventListener("resize", regulatedDetection);
+                    window.removeEventListener("scroll", regulatedDetection);
+                }
                 prevVisibility = isVisible;
                 if (typeof handler == "function")
                     handler(isVisible, elt);
             }
         }
+
+        var regulatedDetection = limitCalls(detectPossibleChange, 20);
         //Bind this internal function with the various events that could cause a change in visibility
-        window.addEventListener("load", detectPossibleChange);
-        window.addEventListener("resize", detectPossibleChange);
-        window.addEventListener("scroll", detectPossibleChange);
+        window.addEventListener("load", regulatedDetection);
+        window.addEventListener("resize", regulatedDetection);
+        window.addEventListener("scroll", regulatedDetection);
     }
 
     /**
-     * Defines a handler to be able to automatically detect when a certain element changes visibility on the page.
+     * Defines a handler to be able to automatically detect when a certain element changes visibility on the page. 
      * @param {object} elt - The element whose visibility we are interested in determining
      * @param {function} handler - The function that will be called when the element has changed its visibility state. It receives a boolean to indicate whether it is visible or not, and a reference to the element that has changed its visibility.
-     * @returns {boolean} Returns true if there is the element is fully visible in the viewport, and returns false if the element disappears (even a little) from the visible area.
+     * @param {boolean} onlyFirstTime - If true the handler is called only the first time it's detected. Otherwise it will be called everytime the element is visible.
      */
-    function inViewportTotally(elt, handler) {
+    function inViewportTotally(elt, handler, onlyFirstTime) {
         var prevVisibility = isElementTotallyVisible(elt);    //Creates a closure for the handler of this particular event
         //Define a handler to determine possible changes
         function detectPossibleChange() {
             var isVisible = isElementTotallyVisible(elt);
             if (isVisible != prevVisibility) { //the visibility state of the element has changed
                 prevVisibility = isVisible;
+                if(onlyFirstTime) { //If it's only to be called the first time, remove the events after it
+                    window.removeEventListener("load", regulatedDetection);
+                    window.removeEventListener("resize", regulatedDetection);
+                    window.removeEventListener("scroll", regulatedDetection);
+                }
                 if (typeof handler == "function")
                     handler(isVisible, elt);
             }
         }
+
+        var regulatedDetection = limitCalls(detectPossibleChange, 20);
         //Bind this internal function with the various events that could cause a change in visibility
-        window.addEventListener("load", detectPossibleChange);
-        window.addEventListener("resize", detectPossibleChange);
-        window.addEventListener("scroll", detectPossibleChange);
+        window.addEventListener("load", regulatedDetection);
+        window.addEventListener("resize", regulatedDetection);
+        window.addEventListener("scroll", regulatedDetection);
     }
     
     //Assign the global helper object
